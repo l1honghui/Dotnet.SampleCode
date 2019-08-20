ddc5eb2ccfaca70f10dea29f3c957e50ff697761
lihonghui <lihonghui1012@outlook.com>
2019-08-20 10:53:56 +0800

    add systeminfo

--- src/SampleCode/Program.cs
+++ src/SampleCode/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace SampleCode
 {
@@ -8,35 +7,13 @@ namespace SampleCode
     {
         public static void Main()
         {
-            string content = "content-length:123";
-            write(int.Parse(content.Substring(15)));
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-            write($"int.Parse(content.Substring(15)) = {int.Parse(content.Substring(15))}");
-            for (int j = 0; j < 100000; j++)
-            {
-                int.Parse(content.Substring(15));
-            }
-            Console.WriteLine("Time Elapsed:" + watch.ElapsedMilliseconds.ToString("N0") + "ms");
-
-            watch.Restart();
-            Span<char> span = content.ToCharArray();
-            write($"int.Parse(span.Slice(15)) = {int.Parse(span.Slice(15))}");
-            for (int j = 0; j < 100000; j++)
-            {
-                int.Parse(span.Slice(15));
-            }
-            Console.WriteLine("Time Elapsed:" + watch.ElapsedMilliseconds.ToString("N0") + "ms");
+            SystemInfos.SystemInfo.PrintSystemInfo();
 
             Console.ReadKey();
         }
-        public static void write(object obj)
+        public static void WriteLine(object obj)
         {
             Console.WriteLine(obj);
-        }
-        public static void write(Span<int> obj)
-        {
-            Console.WriteLine(obj.Length);
         }
 
         /// <summary> 
---
+++ src/SampleCode/SystemInfos/SystemInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SampleCode.SystemInfos
+{
+    class SystemInfo
+    {
+        public static void PrintSystemInfo()
+        {
+            var proc = Process.GetCurrentProcess();
+            var mem = proc.WorkingSet64;
+            var cpu = proc.TotalProcessorTime;
+            
+            //foreach (var aProc in Process.GetProcesses())
+            //    Console.WriteLine("Proc {0,30}  CPU {1,-20:n} msec", aProc.ProcessName, cpu.TotalMilliseconds/Environment.ProcessorCount);
+
+            Console.WriteLine("My process {0} used working set {1:n3} Mb of working set and CPU {2:n} msec",
+                proc.ProcessName, mem / 1024.0 / 1024.0,
+                cpu.TotalMilliseconds / Environment.ProcessorCount);
+        }
+    }
+    
+}
